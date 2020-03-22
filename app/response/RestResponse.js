e1988b52cc6794b15e25c97a4750291083732997
Tobias Dittmann <tobi@Tobiass-iMac.fritz.box>
2020-03-22 22:11:45 +0100

    Added RestResponse module with response wrappers

---
+++ app/response/RestResponse.js
@@ -0,0 +1,43 @@
+exports.success = function(res, msg) {
+    return res.status(200).json({
+        success: true,
+        message: msg
+    });
+};
+
+exports.successData = function(res, msg, data) {
+    return res.status(200).json({
+        success: true,
+        message: msg,
+        data: data
+    });
+};
+
+exports.error = function(res, msg) {
+    return res.status(500).json({
+        success: false,
+        message: msg
+    });
+};
+
+exports.notFound = function(res) {
+    return res.status(404).json({
+        success: false,
+        message: "Page not found"
+    });
+};
+
+exports.invalidData = function(res, msg, data) {
+    return res.status(400).json({
+        success: false,
+        message: msg,
+        data: data
+    });
+};
+
+exports.unauthorized = function(res) {
+    return res.status(401).json({
+        success: false,
+        message: 'Unauthorized'
+    });
+};
