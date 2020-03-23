d6a6f9c398d664975cba41e571bc481ba6f9eb26
Tobias Dittmann <tobi@Tobiass-iMac.fritz.box>
2020-03-23 15:17:30 +0100

    toJSON() method for models

---
+++ model/User.js
@@ -0,0 +1,33 @@
+var mongoose = require("mongoose");
+
+var UserSchema = new mongoose.Schema({
+	firstName: {type: String, required: true},
+	lastName: {type: String, required: true},
+	email: {type: String, required: true},
+	password: {type: String, required: true},
+	isConfirmed: {type: Boolean, required: true, default: 0},
+	confirmKey: {type: String, required:false},
+	status: {type: Boolean, required: true, default: 1}
+}, {timestamps: true});
+
+// Virtual for user's full name
+UserSchema
+	.virtual("fullName")
+	.get(function () {
+		return this.firstName + " " + this.lastName;
+	});
+
+UserSchema.methods = {
+
+    toJSON() {
+        return {
+            id: this._id,
+            firstName: this.firstName,
+            lastName: this.lastName,
+            email: this.email
+        }
+    }
+
+};
+
+module.exports = mongoose.model("User", UserSchema);
